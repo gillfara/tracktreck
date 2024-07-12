c711115cd2addd6b36c3e71df52b09ac99e2c28c
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2024-07-12 05:18:13 +0300

    Modify home layout What is different? -RecentlyPlayedAdapter class to handle the  list of recently played songs. -created RecentlyPlayedItem class to hold the data for each item. -top bar displays the user's name and thumbnail -bottom navigation bar with icons for home, music, and menu - Added RecentlyPlayedAdapter class to handle the binding of data to the RecyclerView for the recently played songs. -item_recently_played.xml defines how each item in the RecyclerView should look. -Moved the media player initialization and setup from MainActivity to MusicActivity -include ExoPlayer setup. But facing issues when compiling,

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,78 +1,78 @@
-package com.example.myapplication;
+    package com.example.myapplication;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
+    import com.loopj.android.http.AsyncHttpClient;
+    import com.loopj.android.http.JsonHttpResponseHandler;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+    import org.json.JSONException;
+    import org.json.JSONObject;
 
-import cz.msebera.android.httpclient.Header;
-import cz.msebera.android.httpclient.entity.StringEntity;
+    import cz.msebera.android.httpclient.Header;
+    import cz.msebera.android.httpclient.entity.StringEntity;
 
-public class LoginActivity extends AppCompatActivity {
+    public class LoginActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
 
-        Button loginBtn = findViewById(R.id.login);
-        EditText usernameView = findViewById(R.id.username);
-        EditText passwordView = findViewById(R.id.password);
-        TextView registerView = findViewById(R.id.create_account);
+            Button loginBtn = findViewById(R.id.login);
+            EditText usernameView = findViewById(R.id.username);
+            EditText passwordView = findViewById(R.id.password);
+            TextView registerView = findViewById(R.id.create_account);
 
-        registerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
+            registerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                }
+            });
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AsyncHttpClient client = new AsyncHttpClient();
-                String username = usernameView.getText().toString();
-                String password = passwordView.getText().toString();
+            loginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AsyncHttpClient client = new AsyncHttpClient();
+                    String username = usernameView.getText().toString();
+                    String password = passwordView.getText().toString();
 
-                if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "You must fill all fields", Toast.LENGTH_LONG).show();
-                } else {
-                    JSONObject params = new JSONObject();
-                    try {
-                        params.put("username", username);
-                        params.put("password", password);
-                        StringEntity entity = new StringEntity(params.toString(), "UTF-8");
-                        client.post(LoginActivity.this, "http://192.168.43.151/login", entity, "application/json", new JsonHttpResponseHandler() {
-                            @Override
-                            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                                if (statusCode == 200) {
+                    if (username.isEmpty() || password.isEmpty()) {
+                        Toast.makeText(LoginActivity.this, "You must fill all fields", Toast.LENGTH_LONG).show();
+                    } else {
+                        JSONObject params = new JSONObject();
+                        try {
+                            params.put("username", username);
+                            params.put("password", password);
+                            StringEntity entity = new StringEntity(params.toString(), "UTF-8");
+                            client.post(LoginActivity.this, "http://192.168.43.151/login", entity, "application/json", new JsonHttpResponseHandler() {
+                                @Override
+                                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                                    if (statusCode == 200) {
 
-                                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
 
+                                    }
                                 }
-                            }
 
-                            @Override
-                            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-                                Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } catch (JSONException e) {
-                        Toast.makeText(LoginActivity.this, "An error occurred", Toast.LENGTH_SHORT).show();
+                                @Override
+                                public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } catch (JSONException e) {
+                            Toast.makeText(LoginActivity.this, "An error occurred", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }
-            }
-        });
+            });
+        }
     }
-}
--- app/src/main/java/com/example/myapplication/RecentlyPlayedItem.java
+++ app/src/main/java/com/example/myapplication/RecentlyPlayedItem.java
@@ -1,2 +1,19 @@
-package com.example.myapplication;public class RecentlyPlayedItem {
+package com.example.myapplication;
+
+public class RecentlyPlayedItem {
+    private String songTitle;
+    private int songThumbnail;
+
+    public RecentlyPlayedItem(String songTitle, int songThumbnail) {
+        this.songTitle = songTitle;
+        this.songThumbnail = songThumbnail;
+    }
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public int getSongThumbnail() {
+        return songThumbnail;
+    }
 }
